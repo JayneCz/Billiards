4747da1a0e8b02b5666e4551b0495a979496d239
Janik Czempin <j.czempin@gmx.de>
2022-10-18 22:47:39 +0200

    create Scoreboard

---
+++ Assets/Scripts/Scoreboard/PointsText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PointsText : ValueText
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindWithTag("Player").GetComponent<CueBallBehaviour>().OnHitObjective += SetPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetPoints(int points)
+    {
+        SetValue(points);
+    }
+}
---
+++ Assets/Scripts/Scoreboard/ShotsText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ShotsText : ValueText
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindWithTag("Player").GetComponent<CueBallBehaviour>().OnShoot += SetShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetShots(int shots)
+    {
+        SetValue(shots);
+    }
+}
