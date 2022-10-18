5caa2477dfe4e04e9a48c5bf8c217181b8ed6993
Janik Czempin <j.czempin@gmx.de>
2022-10-18 22:46:26 +0200

    add objective hit behavior to Cue Ball

--- Assets/Scripts/CueBallBehaviour.cs
+++ Assets/Scripts/CueBallBehaviour.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CueBallBehaviour : MonoBehaviour
 {
+    public event Action<int> OnHitObjective;
+    public event Action<int> OnShoot;
+
     private const string SHOOT_BUTTON_NAME = "Shoot";
+    private const string OBJECTIVE_TAG = "Objective";
 
+    [Header("Shooting")]
     [SerializeField] private Transform cueStick;
-
     [SerializeField] private float shootForceMin = 0f;
     [SerializeField] private float shootForceMax = 100f;
     [SerializeField] private float shootForceSpeed = 1000f;
 
+    [Header("Objectives")]
+    [SerializeField] private int objectiveAmount = 2;
+
+    private int shotCount = 0;
     private float shootForceCurrent = 0f;
+    private readonly ISet<GameObject> hitObjectives = new HashSet<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -35,12 +46,29 @@ public class CueBallBehaviour : MonoBehaviour
             return;
         }
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.CompareTag(OBJECTIVE_TAG))
+        {
+            hitObjectives.Add(other);
+            OnHitObjective?.Invoke(hitObjectives.Count);
+
+            if (hitObjectives.Count >= objectiveAmount)
+            {
+                Debug.Log("You win!");
+            }
+        }
+    }
 
     private void Shoot()
     {
+        OnShoot?.Invoke(++shotCount);
+
         Vector3 force = cueStick.forward;
         force *= shootForceCurrent;
         gameObject.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
         shootForceCurrent = shootForceMin;
     }
+
 }
