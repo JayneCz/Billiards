674bfd7a6fa4a21cb25af594d9882ac400590906
Janik Czempin <j.czempin@gmx.de>
2022-10-20 18:46:14 +0200

    create force slider UI for indicating shot strength

---
+++ Assets/Scripts/UI/ShotForceSlider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShotForceSlider : MonoBehaviour
+{
+    private Slider slider;
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        slider.minValue = player.ShootForceMin;
+        slider.maxValue = player.ShootForceMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = player.ShootForceCurrent;
+    }
+}
