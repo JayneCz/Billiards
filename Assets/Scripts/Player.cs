912edf93f237d19d351f52fbf7e9347e13da0f9e
Janik Czempin <j.czempin@gmx.de>
2022-10-21 18:41:33 +0200

    create replay system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,12 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour
 {
     private const string SHOOT_BUTTON_NAME = "Shoot";
+    private const string REPLAY_BUTTON_NAME = "Replay";
     private const string OBJECTIVE_TAG = "Objective";
+    private const string VICTORY_SCENE_NAME = "Victory";
 
-    public event Action<int> OnPointGain;
-    public event Action<int> OnShoot;
+    public readonly Stats stats = new Stats();
     public float ShootForceCurrent { get; private set; } = 0f;
-
 
     [field: SerializeField] public float ShootForceMin { get; private set; } = 0f;
     [field: SerializeField] public float ShootForceMax { get; private set; } = 25;
@@ -23,12 +23,13 @@ public class Player : MonoBehaviour
     [SerializeField] private Transform cueStick;
     [SerializeField] private int pointsToWin = 3;
 
-    private int shots = 0;
-    private int points = 0;
+    private readonly Dictionary<GameObject, bool> objectivesHit = new Dictionary<GameObject, bool>();
 
     private bool readyToShoot = true;
 
-    private readonly Dictionary<GameObject, bool> objectivesHit = new Dictionary<GameObject, bool>();
+    private bool replaying = false;
+    private Dictionary<GameObject, Vector3> replayObjectPositions;
+    private Vector3 replayForce;
 
     // Start is called before the first frame update
     void Start()
@@ -42,16 +43,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        stats.time += Time.deltaTime;
+
         readyToShoot = IsStationary() && AllObjectivesAreStationary();
 
         if (readyToShoot)
         {
+            replaying = false;
+            if (Input.GetButtonDown(REPLAY_BUTTON_NAME) && stats.shots >= 1 && ShootForceCurrent == ShootForceMin)
+            {
+                StartReplay();
+                return;
+            }
             ChargeShotWithInput();
             ShootWithInput();
         }
     }
     void OnCollisionEnter(Collision collision)
     {
+        if (replaying)
+        {
+            return;
+        }
         GameObject other = collision.gameObject;
         if (other.CompareTag(OBJECTIVE_TAG) && !objectivesHit[other])
         {
@@ -59,11 +72,11 @@ public class Player : MonoBehaviour
 
             if (!objectivesHit.ContainsValue(false))
             {
-                points++;
-                OnPointGain?.Invoke(points);
-                if (points >= pointsToWin)
+                stats.points++;
+                if (stats.points >= pointsToWin)
                 {
-                    Debug.Log("You win!");
+                    stats.Save();
+                    SceneSwitcher.LoadScene(VICTORY_SCENE_NAME);
                 }
             }
         }
@@ -103,10 +116,11 @@ public class Player : MonoBehaviour
 
             ResetObjectivesHit();
 
-            OnShoot?.Invoke(++shots);
+            stats.shots++;
 
             Vector3 force = cueStick.forward;
             force *= ShootForceCurrent;
+            RecordReplay(force);
             gameObject.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
             ShootForceCurrent = ShootForceMin;
         }
@@ -117,6 +131,27 @@ public class Player : MonoBehaviour
         {
             objectivesHit[objective] = false;
         }
+    }
+
+    private void RecordReplay(Vector3 force)
+    {
+        replayForce = force;
+        replayObjectPositions = new Dictionary<GameObject, Vector3>();
+        replayObjectPositions.Add(gameObject, transform.position);
+        foreach (var objective in objectivesHit.Keys)
+        {
+            replayObjectPositions.Add(objective, objective.transform.position);
+        }
+    }
+
+    private void StartReplay()
+    {
+        replaying = true;
+        foreach (var objectPositionPair in replayObjectPositions)
+        {
+            objectPositionPair.Key.transform.position = objectPositionPair.Value;
+        }
+        gameObject.GetComponent<Rigidbody>().AddForce(replayForce, ForceMode.Impulse);
     }
 
 }
